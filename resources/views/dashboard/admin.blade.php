49ed8b236ac623102353cb9278ed7280990e710e
Kennedy Hipolito <41745628+whoamikenken@users.noreply.github.com>
2023-01-19 08:05:57 +0800

    change to s3

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -167,7 +167,7 @@
                                 <div class="row g-0">
                                     <div class="col-4">
                                         @if ($item->user_image)
-                                            <img src="{{  Storage::disk('empsys')->url($item->user_image)}}" class="img-fluid user_photo_list rounded animate__animated animate__fadeIn animate__delay-1s m-2" alt="..." style="height: -webkit-fill-available;" width="120" height="123">
+                                            <img src="{{  Storage::disk('s3')->url($item->user_image)}}" class="img-fluid user_photo_list rounded animate__animated animate__fadeIn animate__delay-1s m-2" alt="..." style="height: -webkit-fill-available;" width="120" height="123">
                                         @else
                                             @if ($item->gender == "male")
                                                 <img src="{{ asset('images/male_sales.png')}}" class="img-fluid user_photo_list rounded animate__animated animate__fadeIn animate__delay-1s" alt="..." width="120" height="123">
