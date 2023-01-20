4071023634f2ce5b4e036bb6f9ea7887d3d32e06
Kennedy Hipolito <41745628+whoamikenken@users.noreply.github.com>
2023-01-20 11:54:26 +0800

    Change table

--- resources/views/logs/web_logs_table.blade.php
+++ resources/views/logs/web_logs_table.blade.php
@@ -4,6 +4,7 @@
         <th class="text-center">Action</th>
         <th class="text-center">Name</th>
         <th class="text-center">Employee ID</th>
+        <th class="text-center">Date Time</th>
         <th class="text-center">IP</th>
         <th class="text-center">Device Type</th>
         <th class="text-center">Image</th>
@@ -17,6 +18,7 @@
         </td>
         <td>{{$item->name}}</td>
         <td>{{$item->employee_id}}</td>
+        <td>{{$item->log_time}}</td>
         <td>{{$item->ip}}</td>
         <td>{{$item->machine_type}}</td>
         <td>
